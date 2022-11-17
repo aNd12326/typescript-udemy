119bf03828bc1e107c33d4a15922f412dc6e847e
Anderson <andersonventosilla0@gmail.com>
2022-11-17 11:01:36 -0500

    seccion genericos done

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,9 +1,6 @@
-import { Hero } from "./classes/Hero";
-// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
-// import * as HeroClasses from "./classes/Hero";
+import { getPokemon } from "./generics/get-pokemon";
 
-// const ironMan = new SuperHero("ironman", 1, 55);
-const ironMan = new Hero("ironman", 1, 55);
-
-console.log(ironMan);
-console.log(ironMan.power);
+getPokemon(4)
+  .then((pokemon) => console.log(pokemon.sprites.front_default))
+  .catch((err) => console.log(err))
+  .finally(() => console.log("Fin de getPokemon"));
